f1e01ea8562b64c263c50178fd61b09be0c0d5d7
Padmasing <padmasingnawale@gmail.com>
2025-07-02 16:44:35 +0530

    Package 1 programs

---
+++ Teacher.java
@@ -0,0 +1,21 @@
+package package1;
+
+public class Teacher {
+	
+	public static void add() {
+
+	}
+
+	private static void sub() {
+
+	}
+
+	static void mul() {
+
+	}
+
+	protected static void div() {
+
+	}
+
+}
---
+++ Teacher1.java
@@ -0,0 +1,21 @@
+package package1;
+
+public class Teacher1 {
+	
+	public static void add() {
+
+	}
+
+	private static void sub() {
+
+	}
+
+	static void mul() {
+
+	}
+
+	protected static void div() {
+
+	}
+
+}
