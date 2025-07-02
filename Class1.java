04ad314025042650a19d93b0e26ef7b784374cbc
Padmasing <padmasingnawale@gmail.com>
2025-07-02 12:07:38 +0530

    my 3rd commit

---
+++ Class1.java
@@ -0,0 +1,15 @@
+package ab55NonStatic;
+
+public class Class1 {
+
+	void login() {
+
+		System.out.println("login to amazon");
+	}
+
+	void logout() {
+
+		System.out.println("logout from amazon");
+	}
+
+}
---
+++ Class2.java
@@ -0,0 +1,14 @@
+package ab55NonStatic;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		
+		  Class1 c1=new Class1();
+		  
+		  c1.login();
+		  c1.logout();
+		
+	}
+
+}
